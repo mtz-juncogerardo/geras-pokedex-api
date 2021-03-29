6f87515d49f146d8f18c930ffb79f33c6d0ba43a
Gerardo Martínez <mtz.juncogerardo@gmail.com>
2021-03-29 00:01:18 -0600

    Initial Setup

---
+++ Models/FavoriteItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace geras_pokemon_api.Models
+{
+    public class FavoriteItem
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string PokemonId { get; set; }
+        public PokemonItem Pokemon { get; set; }
+        public UserItem User { get; set; }
+    }
+}
---
+++ Models/UserItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace geras_pokemon_api.Models
+{
+    public class UserItem
+    {
+    }
+}
